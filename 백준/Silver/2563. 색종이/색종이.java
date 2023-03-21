40964ec409239b60df4583fff2d19355a3db48e4
jooyoun lee <115621310+zwzwzw123@users.noreply.github.com>
2023-03-21 21:41:47 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -9,22 +9,23 @@ public class Main {
 		// 검은 영역의 넓이
 		int total = 0;
 		int n = Integer.parseInt(br.readLine());// 색종이 갯수
-		boolean[][] arr = new boolean[101][101];// 도화지 크기
+		int[][] arr = new int[100][100];// 도화지 크기
 
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
-
-			// (x,y)부터 (x+9, y+9)까지의 영역을 하나씩 체크한 후 총 넓이에 더해줌
-			for (int j = x; j < x + 10; j++) {
-				for (int k = y; k < y + 10; k++) {
-					if (!arr[j][k]) {
-						arr[j][k] = true;
-						total++;
-					}
-
+			
+			for(int j =x;j<x+10;j++) {
+				for(int k =y;k<y+10;k++) {
+					arr[j][k] =1;
 				}
+			}
+		}
+		
+		for(int i= 0;i<100;i++) {
+			for(int j =0;j<100;j++) {
+				if(arr[i][j]==1) total++;
 			}
 		}
 		System.out.println(total);
