322cdcc32ea51f27b675a7e54d42ad910085e791
jooyoun lee <115621310+zwzwzw123@users.noreply.github.com>
2023-03-23 15:33:56 +0900

    [Bronze III] Title: 배수와 약수, Time: 116 ms, Memory: 14000 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -1,22 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.time.chrono.ThaiBuddhistEra;
 import java.util.StringTokenizer;
+
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		
-		
-		String f = "factor\n";
-		String m = "multiple\n";
-		String n = "neither\n";
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
 		while(true) {
+			
 			st = new StringTokenizer(br.readLine());
 			
 			int first = Integer.parseInt(st.nextToken());
@@ -24,18 +19,10 @@ public class Main {
 			
 			if(first == 0 && second == 0) break;
 			
-			//첫번째 수, 두번째 수 약수 일때
-			if(second % first == 0) sb.append(f);
-			
-			//첫번째 수, 두번째 수 배수 일때
-			else if(first % second == 0) sb.append(m);
-			
-			//약수, 배수가 아닐 때
-			else sb.append(n);
+			if(second % first ==0) System.out.println("factor");
+			else if(first % second ==0) System.out.println("multiple");
+			else System.out.println("neither");
 		}
-		
-		System.out.println(sb);
-		
 	}
 
 }
