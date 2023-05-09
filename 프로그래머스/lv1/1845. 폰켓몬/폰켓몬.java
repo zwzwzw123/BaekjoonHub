820bb4f07f4b4c55622f8ce980c7c0970658980f
jooyoun lee <115621310+zwzwzw123@users.noreply.github.com>
2023-05-09 14:56:23 +0900

    [level 1] Title: 폰켓몬, Time: 1.98 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int max = nums.length /2;
+        
+        HashSet<Integer> numsSet = new HashSet<>();
+            
+         for(int num : nums){
+             numsSet.add(num);
+         }   
+        
+        if(numsSet.size()>max){
+            return max;
+        }else{
+            return numsSet.size();
+        }
+        
+
+    }
+}
