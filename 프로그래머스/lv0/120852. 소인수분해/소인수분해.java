f04e95ee9fe51705e279b1ae15694eee55f068cf
jooyoun lee <115621310+zwzwzw123@users.noreply.github.com>
2023-05-14 17:22:30 +0900

    [level 0] Title: 소인수분해, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -1,9 +1,8 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n) {
-        List<Integer> list = new ArrayList<>();
-        
-        for(int i =2;i<=n;i++){
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=2;i<=n;i++){
             if(n%i==0){
                 while(n%i==0){
                     n/=i;
@@ -13,9 +12,8 @@ class Solution {
         }
         int[] answer = new int[list.size()];
         for(int i =0;i<list.size();i++){
-            answer[i]=list.get(i);
+            answer[i] = list.get(i);
         }
-       
         return answer;
     }
 }
