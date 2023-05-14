f888678b3598ff8520fa96280c7cf068fff55bd7
jooyoun lee <115621310+zwzwzw123@users.noreply.github.com>
2023-05-14 16:33:01 +0900

    [level 0] Title: 모음 제거, Time: 0.11 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,10 +1,7 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = my_string;
-        String[]arr = {"a","e","i","o","u"};
-        for(String a: arr){
-            answer = answer.replaceAll(a,"");
-        }
+        String answer = "";
+        answer = my_string.replaceAll("[aeiou]","");
         return answer;
     }
 }
